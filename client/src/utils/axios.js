82f9ec0edb154343593a6ad05005f37f48e14c5e
scrubandmore <scrubandmoreskincare@gmail.com>
2025-12-27 22:13:58 +0530

    Move rewrites to client and remove server config

--- client/src/utils/axios.js
+++ client/src/utils/axios.js
@@ -1,7 +1,9 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: import.meta.env.VITE_API_URL || "https://scrubandmore.onrender.com",
+  baseURL:
+    import.meta.env.VITE_API_URL ||
+    "https://scrubandmore.onrender.com/api",
   withCredentials: true,
 });
 
