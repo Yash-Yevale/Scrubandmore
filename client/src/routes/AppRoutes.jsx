975200011de01ae1fadb39c5e8a8606d1e7edf1f
scrubandmore <scrubandmoreskincare@gmail.com>
2025-12-30 00:52:56 +0530

    Add admin route and dashboard

--- client/src/routes/AppRoutes.jsx
+++ client/src/routes/AppRoutes.jsx
@@ -14,46 +14,42 @@ import { Contact } from "../pages/contact/contact";
 import { SingleProduct } from "../pages/product/SingleProduct";
 import { RefundPolicy } from "../pages/RefundPolicy";
 
+// 🚩 ADMIN
+import AdminDashboard from "../pages/admin/AdminDashboard";
 
 export const AppRoutes = () => {
   return (
     <>
-      {/* NAVBAR */}
       <Navbar />
 
-      {/* ROUTES */}
       <Routes>
-        {/* HOME */}
         <Route path="/" element={<Home />} />
 
-        {/* PRODUCTS */}
         <Route path="/allProducts" element={<Products />} />
         <Route path="/men" element={<Products />} />
         <Route path="/women" element={<Products />} />
         <Route path="/kids" element={<Products />} />
+
         <Route path="/product/:id" element={<SingleProduct />} />
 
-        {/* INFO */}
         <Route path="/description" element={<Description />} />
         <Route path="/about" element={<About />} />
         <Route path="/contact" element={<Contact />} />
 
-        {/* CART & CHECKOUT */}
         <Route path="/cart" element={<Cart />} />
         <Route path="/checkout" element={<Checkout />} />
 
-        {/* OPTIONAL */}
         <Route path="/favourite" element={<Favourite />} />
         <Route path="/orders" element={<Order />} />
 
-        {/* FALLBACK */}
-        <Route path="*" element={<Home />} />
+        {/* ADMIN PANEL */}
+        <Route path="/admin" element={<AdminDashboard />} />
 
         <Route path="/refund-policy" element={<RefundPolicy />} />
 
+        <Route path="*" element={<Home />} />
       </Routes>
 
-      {/* FOOTER */}
       <Footer />
     </>
   );
